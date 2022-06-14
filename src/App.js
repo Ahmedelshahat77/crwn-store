89d44da13a49470cb6ebe497633ec75970641e31
ahmed shehta <Ahmedelshahat0077@gmail.com>
2022-06-14 13:30:30 +0200

    update the shop page with items perview

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
-const HatPage = () => (
-  <div>
-    <h1>Hat Page </h1>
-  </div>
-);
-const Jackets = () => (
-  <div>
-    <h1>Jackets Page </h1>
-  </div>
-);
+import ShopPage from "./pages/homepage/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="hats" element={<HatsPage />} />
-        <Route path=":hatsId" element={<HatPage />} />
-        <Route path="jackets" element={<Jackets />} />
+        <Route path="shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
