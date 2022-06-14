92a9b1e4e78a90358346d0512ffc726654378a6b
ahmed shehta <Ahmedelshahat0077@gmail.com>
2022-06-14 20:45:25 +0200

    create login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/homepage/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInSignUpPage from "./pages/sign-in-and-sign-up/sign-in-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="shop" element={<ShopPage />} />
+        <Route path="signin" element={<SignInSignUpPage />} />
       </Routes>
     </div>
   );
